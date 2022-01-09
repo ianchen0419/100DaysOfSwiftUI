3bd6613e346197cedd1e728a5afba72b28b6a2a9
Ian <ianchen0419@gmail.com>
2022-01-09 21:14:03 +0800

    Create Day2-How-to-store-truth-with-Booleans.swift

---
+++ Day2-How-to-store-truth-with-Booleans.swift
@@ -0,0 +1,12 @@
+import Cocoa
+
+let goodDogs = true
+let isMultiple = 120.isMultiple(of: 3)
+
+var isAuthenticated = false
+isAuthenticated = !isAuthenticated
+print(isAuthenticated)
+
+var gameOver = false
+gameOver.toggle()
+print(gameOver)
