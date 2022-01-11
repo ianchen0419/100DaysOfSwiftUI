3bd5f8f3e5bfcbf969aa35fabd6d3da4e929e067
Ian <ianchen0419@gmail.com>
2022-01-11 09:36:21 +0800

    Create Day3-How-to-store-and-find-data-in-dictionaries.swift

---
+++ Day3-How-to-store-and-find-data-in-dictionaries.swift
@@ -0,0 +1,35 @@
+import Cocoa
+
+let employee2 = [
+    "name": "Taylor Swift",
+    "job": "Singer",
+    "location": "Nashville",
+]
+//left: dictionary key
+//right: dictionary value
+print(employee2["name", default: "Unknown"])
+print(employee2["name2", default: "Unknown"])
+
+let hasGraduated = [
+    "Eric": false,
+    "Maeve": true,
+    "Otis": false,
+]
+
+let olympics = [
+    2012: "London",
+    2016: "Rio de Janeiro",
+    2121: "Tokyo",
+]
+
+print(olympics[2012, default: "Unknown"])
+
+var heights = [String: Int]()
+heights["Yao Ming"] = 229
+heights["Shaquille O'Neal"] = 216
+heights["LeBron James"] = 206
+
+var archEnemies = [String: String]()
+archEnemies["Batman"] = "The Joker"
+archEnemies["Superman"] = "Lex Luthor"
+archEnemies["Batman"] = "Penguin"
