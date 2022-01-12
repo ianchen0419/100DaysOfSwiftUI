085cf1562a380977ded6a71ae477c79a8980f74c
Ian <ianchen0419@gmail.com>
2022-01-12 17:02:12 +0800

    Create Day5-check-multiple-conditions.swift

---
+++ Day5-check-multiple-conditions.swift
@@ -0,0 +1,38 @@
+import Cocoa
+
+let age = 16
+
+if age >= 18 {
+    print("You can vote in the next election.")
+} else {
+    print("Sorry, you're too young to vote.")
+}
+
+let temp = 25
+
+if temp > 20 && temp < 30 {
+    print("It's a nice day.")
+}
+
+let userAge = 14
+let hasParentalConsent = true
+
+if age >= 18 || hasParentalConsent {
+    print("You can buy the game!")
+}
+
+enum TransportOption {
+    case airpline, helicopter, bicycle, car, escooter
+}
+
+let transport = TransportOption.airpline
+
+if transport == .airpline || transport == .helicopter {
+    print("Let's fly!")
+} else if transport == .bicycle {
+    print("I hope there's a bile path...")
+} else if transport == .car {
+    print("Time to get stuck in traffic.")
+} else {
+    print("I'm going to hire a scooter now!")
+}
