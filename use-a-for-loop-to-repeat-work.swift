71008d8e0c0a41dc60afce6394d003a45a2039d0
Ian <ianchen0419@gmail.com>
2022-01-15 14:57:44 +0800

    Create use-a-for-loop-to-repeat-work.swift

---
+++ use-a-for-loop-to-repeat-work.swift
@@ -0,0 +1,31 @@
+import Cocoa
+
+let platforms = ["iOS", "macOS", "tvOS", "watchOS"]
+
+for os in platforms {
+    print("Swift works great on \(os).")
+}
+
+//1...12: range
+for i in 1...12 {
+    print("The \(i) times table")
+    for j in 1...12 {
+        print("   \(j) x \(i) is \(j * i)")
+    }
+}
+
+//1 though 5
+for i in 1...5 {
+    print("Counting from 1 throung 5: \(i)")
+}
+
+//1 up to 5
+for i in 1..<5 {
+    print("Counting from 1 throung 5: \(i)")
+}
+
+var lyric = "Haters gonna"
+for _ in 1...5 {
+    lyric += " hate"
+}
+print(lyric)
