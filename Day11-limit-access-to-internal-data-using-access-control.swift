90cd44b7186f4cee90b0188e914f1e67099c7ddb
Ian <ianchen0419@gmail.com>
2022-01-21 20:50:00 +0800

    Update Day11-limit-access-to-internal-data-using-access-control.swift

--- Day11-limit-access-to-internal-data-using-access-control.swift
+++ Day11-limit-access-to-internal-data-using-access-control.swift
@@ -8,7 +8,7 @@ import Cocoa
  */
 
 struct BankAccount {
-    private(set) var funds = 0 //access control
+    private var funds = 0 //access control
     
     mutating func deposit(amount: Int) {
         funds += amount
@@ -33,5 +33,32 @@ if success {
 } else {
     print("Failed to get the money")
 }
-print(account.funds)
+//print(account.funds)
 //account.funds -= 1000 //without private funds property is exposed externally
+
+//Swift don't neet to init property, because all property is initialized beforehand (all default value is filled)
+struct Dog1 {
+    var name: String = "Moo" //no need to init
+}
+let moko1 = Dog1()
+
+struct Dog2 {
+    private var name: String = "Moo" //no need to init
+}
+let moko2 = Dog2()
+
+//Swift NEED to init property, because one of these isn't asssigned value beforehandly
+struct Dog3 {
+    var name: String //need to init (because of no default value)
+}
+let moko3 = Dog3(name: "Moko")
+
+//Swift need al init all property except wih private let (because Swift need to init var name, he also need to init others except with private let)
+struct Dog4 {
+    //private var age: Int = 6 //need to init (because of var name need init)
+    private let age: Int = 6 //no need to init (becaues he is private let)
+    var name: String //need to init (because of no default value)
+}
+let moko4 = Dog4(name: "Moko")
+
+
