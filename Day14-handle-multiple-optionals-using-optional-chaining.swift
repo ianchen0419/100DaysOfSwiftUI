200e2743d991b435d4612723b11d7cb0adb86bd2
Ian <ianchen0419@gmail.com>
2022-01-27 00:49:44 +0800

    Update Day14-handle-multiple-optionals-using-optional-chaining.swift

--- Day14-handle-multiple-optionals-using-optional-chaining.swift
+++ Day14-handle-multiple-optionals-using-optional-chaining.swift
@@ -2,6 +2,7 @@ import Cocoa
 
 let names = ["Arya", "Bran", "Robb", "Sansa"]
 let chosen = names.randomElement()?.uppercased() ?? "No one"
+//let chosen = (names.randomElement()?.uppercased()) ?? "No one"
 print("Next in line: \(chosen)")
 //optional chaining: if the optional has value, run some new code at that value
 //If the optional has a value inside, unwrap it then ...
