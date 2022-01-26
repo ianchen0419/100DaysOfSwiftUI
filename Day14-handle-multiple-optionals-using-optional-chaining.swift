503582540e6606bd15e1c48c08a3fa42410f91bc
Ian <ianchen0419@gmail.com>
2022-01-26 21:52:29 +0800

    Create Day14-handle-multiple-optionals-using-optional-chaining.swift

---
+++ Day14-handle-multiple-optionals-using-optional-chaining.swift
@@ -0,0 +1,16 @@
+import Cocoa
+
+let names = ["Arya", "Bran", "Robb", "Sansa"]
+let chosen = names.randomElement()?.uppercased() ?? "No one"
+print("Next in line: \(chosen)")
+//optional chaining: if the optional has value, run some new code at that value
+//If the optional has a value inside, unwrap it then ...
+
+struct Book {
+    let title: String
+    let author: String?
+}
+
+var book: Book? = nil
+let author = book?.author?.first?.uppercased() ?? "A"
+print(author)
