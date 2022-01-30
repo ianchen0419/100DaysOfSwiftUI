0211bc277a1bf1dbaaebc21224f11f43bd9a525e
Ian <ianchen0419@gmail.com>
2022-01-30 18:43:32 +0800

    Create Day20-using-stacks-to-arrange-views.swift

---
+++ Day20-using-stacks-to-arrange-views.swift
@@ -0,0 +1,38 @@
+//
+//  ContentView.swift
+//  GuessTheFlag
+//
+//  Created by Yi An Chen on 2022/1/30.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        
+        VStack {
+            HStack {
+                Text("1")
+                Text("2")
+                Text("3")
+            }
+            HStack {
+                Text("4")
+                Text("5")
+                Text("6")
+            }
+            HStack {
+                Text("7")
+                Text("8")
+                Text("9")
+            }
+        }
+        
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
