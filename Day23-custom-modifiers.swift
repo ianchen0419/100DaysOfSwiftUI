bb51380d5331e29d1c6b953c1da602635fcb3eb5
Ian <ianchen0419@gmail.com>
2022-01-31 23:01:52 +0800

    Create Day23-custom-modifiers.swift

---
+++ Day23-custom-modifiers.swift
@@ -0,0 +1,69 @@
+//
+//  ContentView.swift
+//  ViewsAndModifiers
+//
+//  Created by Yi An Chen on 2022/1/31.
+//
+
+import SwiftUI
+
+struct Watermark: ViewModifier {
+    var text: String
+    
+    func body(content: Content) -> some View {
+        ZStack(alignment: .bottomTrailing) {
+            content
+            
+            Text(text)
+                .font(.caption)
+                .foregroundColor(.white)
+                .padding(5)
+                .background(.black)
+        }
+    }
+}
+
+extension View {
+    func watermarked(with text: String) -> some View {
+        modifier(Watermark(text: text))
+    }
+}
+
+struct Title: ViewModifier {
+    func body(content: Content) -> some View {
+        content
+            .font(.largeTitle)
+            .foregroundColor(.white)
+            .padding()
+            .background(.blue)
+            .clipShape(RoundedRectangle(cornerRadius: 10))
+    }
+}
+
+extension View {
+    func titleStyle() -> some View {
+        modifier(Title())
+    }
+}
+
+struct ContentView: View {
+    
+    var body: some View {
+        
+//        Text("Hello, world!")
+//            .modifier(Title())
+        
+//        Text("Hello, world!")
+//            .titleStyle()
+        
+        Color.blue
+            .frame(width: 300, height: 200)
+            .watermarked(with: "Hacking with Swift")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
