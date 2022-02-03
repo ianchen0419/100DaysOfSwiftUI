21abe63d32248d0cb2d07783021fbc060f36e06a
Ian <ianchen0419@gmail.com>
2022-02-03 18:51:33 +0800

    Create Day32-creating-implicit-animations.swift

---
+++ Day32-creating-implicit-animations.swift
@@ -0,0 +1,32 @@
+//
+//  ContentView.swift
+//  Animations
+//
+//  Created by Yi An Chen on 2022/2/3.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    
+    @State var animationAmount = 1.0
+    
+    var body: some View {
+        Button("Tap Me") {
+            animationAmount += 1
+        }
+        .padding(50)
+        .background(.red)
+        .foregroundColor(.white)
+        .clipShape(Circle())
+        .scaleEffect(animationAmount)
+        .blur(radius: (animationAmount - 1) * 3)
+        .animation(.default, value: animationAmount)
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
