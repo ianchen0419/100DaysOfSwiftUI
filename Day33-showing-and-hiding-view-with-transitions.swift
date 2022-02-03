150a9c11e1313cf02bdef8309dd9f559a933b0fc
Ian <ianchen0419@gmail.com>
2022-02-03 21:33:26 +0800

    Create Day33-showing-and-hiding-view-with-transitions.swift

---
+++ Day33-showing-and-hiding-view-with-transitions.swift
@@ -0,0 +1,41 @@
+//
+//  ContentView.swift
+//  Animations
+//
+//  Created by Yi An Chen on 2022/2/3.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    
+    @State private var isShowingRed = false
+    
+    var body: some View {
+        
+        VStack {
+            Button("Tap Me") {
+                withAnimation {
+                    isShowingRed.toggle()
+                }
+                
+            }
+            
+            if isShowingRed {
+                Rectangle()
+                    .fill(.red)
+                    .frame(width: 200, height: 200)
+//                    .transition(.scale)
+                    .transition(.asymmetric(insertion: .scale, removal: .opacity))
+            }
+            
+        }
+        
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
