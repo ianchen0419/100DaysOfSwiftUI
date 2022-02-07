d3a4c003c8e23cc777a18fb0d72e40e4e0a3ebaf
Ian <ianchen0419@gmail.com>
2022-02-07 20:40:54 +0800

    Create Day39-working-with-hierarchical-codable-data.swift

---
+++ Day39-working-with-hierarchical-codable-data.swift
@@ -0,0 +1,46 @@
+//
+//  ContentView.swift
+//  Moonshot
+//
+//  Created by Yi An Chen on 2022/2/7.
+//
+
+import SwiftUI
+
+struct User: Codable {
+    let name: String
+    let address: Address
+}
+
+struct Address: Codable {
+    let street: String
+    let city: String
+}
+
+struct ContentView: View {
+    var body: some View {
+        Button("Decode JSON") {
+            let input = """
+            {
+                "name": "Taylor Swift",
+                "address": {
+                    "street": "555, Taylor Swift Avenue",
+                    "city": "Nashiville"
+                }
+            }
+            """
+            
+            let data = Data(input.utf8)
+            
+            if let user = try? JSONDecoder().decode(User.self, from: data) {
+                print(user.address.street)
+            }
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
