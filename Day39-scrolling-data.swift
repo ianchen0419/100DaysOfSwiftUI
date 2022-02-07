7ea7d7c9328a392d87ac324acc24581bce8928f5
Ian <ianchen0419@gmail.com>
2022-02-07 18:25:29 +0800

    Create Day39-scrolling-data.swift

---
+++ Day39-scrolling-data.swift
@@ -0,0 +1,59 @@
+//
+//  ContentView.swift
+//  Moonshot
+//
+//  Created by Yi An Chen on 2022/2/7.
+//
+
+import SwiftUI
+
+struct CustomText: View {
+    let text: String
+    
+    var body: some View {
+        Text(text)
+    }
+    
+    init(_ text: String) {
+        print("Creating a new CustomText")
+        self.text = text
+    }
+}
+
+struct ContentView: View {
+    var body: some View {
+        ScrollView {
+//            VStack(spacing: 10) {
+//                ForEach(0..<100) {
+//                    CustomText("Item \($0)")
+//                        .font(.title)
+//                }
+//            }
+//            .frame(maxWidth: .infinity)
+            
+            LazyVStack(spacing: 10) {
+                ForEach(0..<100) {
+                    CustomText("Item \($0)")
+                        .font(.title)
+                }
+            }
+            
+        }
+        
+        
+//        ScrollView(.horizontal) {
+//            LazyHStack(spacing: 10) {
+//                ForEach(0..<100) {
+//                    CustomText("Item \($0)")
+//                        .font(.title)
+//                }
+//            }
+//        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
