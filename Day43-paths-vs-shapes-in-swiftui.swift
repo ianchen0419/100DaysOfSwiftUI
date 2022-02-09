489e0c90ea26f1f39fc7f43b441a6bd5b48490e0
Ian <ianchen0419@gmail.com>
2022-02-10 00:24:38 +0800

    Create Day43-paths-vs-shapes-in-swiftui.swift

---
+++ Day43-paths-vs-shapes-in-swiftui.swift
@@ -0,0 +1,58 @@
+//
+//  ContentView.swift
+//  Drawing
+//
+//  Created by Yi An Chen on 2022/2/9.
+//
+
+import SwiftUI
+
+struct Triangle: Shape {
+    func path(in rect: CGRect) -> Path {
+        var path = Path()
+        
+        path.move(to: CGPoint(x: rect.midX, y: rect.minY))
+        path.addLine(to: CGPoint(x: rect.minX, y: rect.maxY))
+        path.addLine(to: CGPoint(x: rect.maxX, y: rect.maxY))
+        path.addLine(to: CGPoint(x: rect.midX, y: rect.minY))
+        
+        return path
+    }
+}
+
+struct Arc: Shape {
+    let startAngle: Angle
+    let endAngle: Angle
+    let clockwise: Bool
+    
+    func path(in rect: CGRect) -> Path {
+        let rotationAdjustment = Angle.degrees(90)
+        let modifiedStart = startAngle - rotationAdjustment
+        let modifiedEnd = endAngle - rotationAdjustment
+        
+        var path = Path()
+        
+        path.addArc(center: CGPoint(x: rect.midX, y: rect.midY), radius: rect.width / 2, startAngle: modifiedStart, endAngle: modifiedEnd, clockwise: !clockwise)
+        
+        return path
+    }
+}
+
+struct ContentView: View {
+    var body: some View {
+//        Triangle()
+//            .stroke(.red, style: StrokeStyle(lineWidth: 10, lineCap: .round, lineJoin: .round))
+//            .fill(.red)
+//            .frame(width: 300, height: 300)
+        
+        Arc(startAngle: .degrees(0), endAngle: .degrees(180), clockwise: true)
+            .stroke(.blue, lineWidth: 10)
+            .frame(width: 300, height: 300)
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
