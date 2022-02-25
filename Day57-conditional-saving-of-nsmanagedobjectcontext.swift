a19acf58acdabbbe761bbceeaae56c301817e8e0
Ian <ianchen0419@gmail.com>
2022-02-25 23:04:03 +0800

    Create Day57-conditional-saving-of-nsmanagedobjectcontext.swift

---
+++ Day57-conditional-saving-of-nsmanagedobjectcontext.swift
@@ -0,0 +1,27 @@
+//
+//  ContentView.swift
+//  CoreDataProject
+//
+//  Created by Yi An Chen on 2022/2/23.
+//
+
+import SwiftUI
+
+
+struct ContentView: View {
+    @Environment(\.managedObjectContext) var moc
+    
+    var body: some View {
+        Button("Save") {
+            if moc.hasChanges {
+                try? moc.save()
+            }
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
