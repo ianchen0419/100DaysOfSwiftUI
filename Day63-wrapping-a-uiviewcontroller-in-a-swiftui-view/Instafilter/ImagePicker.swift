b87a66e63f2aa1218aeeb2e058e7199253325aa2
Yi An Chen <ianchen0419@gmail.com>
2022-03-09 20:18:11 +0800

    update

--- Day63-wrapping-a-uiviewcontroller-in-a-swiftui-view/Instafilter/ImagePicker.swift
+++ Day63-wrapping-a-uiviewcontroller-in-a-swiftui-view/Instafilter/ImagePicker.swift
@@ -9,11 +9,38 @@ import PhotosUI
 import SwiftUI
 
 struct ImagePicker: UIViewControllerRepresentable {
+    @Binding var image: UIImage?
+    
+    class Coordinator: NSObject, PHPickerViewControllerDelegate {
+        // NSObject is th based object of all UIKit hierarchy, the picker can say: hey the user chose an image, what do you want to do
+        // PHPickerViewControllerDelegate will add functionality for detecting when these are chosen image
+        
+        var parent: ImagePicker
+        
+        init(_ parent: ImagePicker) {
+            self.parent = parent
+        }
+        
+        func picker(_ picker: PHPickerViewController, didFinishPicking results: [PHPickerResult]) {
+            picker.dismiss(animated: true)
+            
+            guard let provider = results.first?.itemProvider else { return }
+            
+            if provider.canLoadObject(ofClass: UIImage.self) {
+                provider.loadObject(ofClass: UIImage.self) { image, _ in
+                    self.parent.image = image as? UIImage
+                }
+            }
+        }
+        
+    }
+    
     func makeUIViewController(context: Context) -> PHPickerViewController {
         var config = PHPickerConfiguration()
         config.filter = .images
         
         let picker = PHPickerViewController(configuration: config)
+        picker.delegate = context.coordinator
         return picker
     }
     
@@ -22,4 +49,8 @@ struct ImagePicker: UIViewControllerRepresentable {
     }
     
     // typealias UIViewControllerType = PHPickerViewController
+    
+    func makeCoordinator() -> Coordinator {
+        Coordinator(self) // talks between UIKit and SwiftUI
+    }
 }
