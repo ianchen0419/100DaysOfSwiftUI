9d937ff9391ec6f03bc1529ca4782b6678bec27b
Ian <ianchen0419@gmail.com>
2022-03-11 16:49:46 +0800

    Create Day69-using-touch-id-and-face-id-with-swiftui.swift

---
+++ Day69-using-touch-id-and-face-id-with-swiftui.swift
@@ -0,0 +1,51 @@
+//
+//  ContentView.swift
+//  BucketList
+//
+//  Created by Yi An Chen on 2022/3/10.
+//
+
+import LocalAuthentication
+import SwiftUI
+
+struct ContentView: View {
+    @State private var isUnlocked = false
+    
+    var body: some View {
+        VStack {
+            if isUnlocked {
+                Text("Unlocked")
+            } else {
+                Text("Locked")
+            }
+        }
+        .onAppear(perform: authenticate)
+    }
+    
+    func authenticate() {
+        let context = LAContext()
+        var error: NSError?
+        
+        if context.canEvaluatePolicy(.deviceOwnerAuthentication, error: &error) {
+            let reason = "We need to unlock your data"
+            
+            context.evaluatePolicy(.deviceOwnerAuthenticationWithBiometrics, localizedReason: reason) { success, authenticationError in
+                if success {
+                    // authenticated successfully
+                    isUnlocked = true
+                } else {
+                    // there was a problem
+                }
+            }
+        } else {
+            // no biometrics
+        }
+    }
+    
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
