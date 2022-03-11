e0b89e666eb164156884604d0db276b5300a52a4
Ian <ianchen0419@gmail.com>
2022-03-11 15:11:06 +0800

    Create Day68-writing-data-to-the-documents-directory.swift

---
+++ Day68-writing-data-to-the-documents-directory.swift
@@ -0,0 +1,39 @@
+//
+//  ContentView.swift
+//  BucketList
+//
+//  Created by Yi An Chen on 2022/3/10.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    
+    var body: some View {
+        Text("Hello, world!")
+            .onTapGesture {
+                let str = "Test Message"
+                let url = getDocumentsDirectory().appendingPathComponent("message.txt")
+                
+                do {
+                    try str.write(to: url, atomically: true, encoding: .utf8)
+                    
+                    let input = try String(contentsOf: url)
+                    print(input)
+                } catch {
+                    print(error.localizedDescription)
+                }
+            }
+    }
+    
+    func getDocumentsDirectory() -> URL {
+        let paths = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask)
+        return paths[0]
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
