42ac8d06dd82d5d50af257c5efb89a4edeec34fc
Ian <ianchen0419@gmail.com>
2022-03-19 17:53:39 +0800

    Create Day81-scheduling-local-notifications.swift

---
+++ Day81-scheduling-local-notifications.swift
@@ -0,0 +1,46 @@
+//
+//  ContentView.swift
+//  HotProspects
+//
+//  Created by Yi An Chen on 2022/3/18.
+//
+
+import SwiftUI
+import UserNotifications
+
+struct ContentView: View {
+    
+    var body: some View {
+        VStack {
+            Button("Request Permission") {
+                UNUserNotificationCenter.current().requestAuthorization(options: [.alert, .badge, .sound]) { success, error in
+                    if success {
+                        print("All set!")
+                    } else if let error = error {
+                        print(error.localizedDescription)
+                    }
+                }
+            }
+            
+            Button("Schedule Notification") {
+                let content = UNMutableNotificationContent()
+                content.title = "Feed the dogs"
+                content.subtitle = "They look hungry"
+                content.sound = UNNotificationSound.default
+                
+                let trigger = UNTimeIntervalNotificationTrigger(timeInterval: 5, repeats: false)
+                
+                let request = UNNotificationRequest(identifier: UUID().uuidString, content: content, trigger: trigger)
+                
+                UNUserNotificationCenter.current().add(request)
+            }
+        }
+    }
+    
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
