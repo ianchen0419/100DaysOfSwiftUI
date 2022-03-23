0311d5fa6d33b723a9631ed690f249a482904d54
Ian <ianchen0419@gmail.com>
2022-03-23 23:12:51 +0800

    Create Day94-challenge.swift

---
+++ Day94-challenge.swift
@@ -0,0 +1,42 @@
+//
+//  ContentView.swift
+//  LayoutAndGeometry
+//
+//  Created by Yi An Chen on 2022/3/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    
+    var body: some View {
+        GeometryReader { fullView in
+            ScrollView(.vertical) {
+                ForEach(0..<50) { index in
+                    GeometryReader { geo in
+                        Text("Row #\(index)")
+                            .font(.title)
+                            .frame(maxWidth: .infinity)
+                            .background(Color(
+                                hue: geo.frame(in: .global).minY / fullView.size.height ,
+                                saturation: 1,
+                                brightness: 1,
+                                opacity: (geo.frame(in: .global).minY - fullView.safeAreaInsets.top) * 0.005
+                            ))
+                            .rotation3DEffect(.degrees(geo.frame(in: .global).minY - fullView.size.height / 2) / 5, axis: (x: 0, y: 1, z: 0))
+                            .scaleEffect(scaleValue(geo: geo, fullView: fullView), anchor: .trailing)
+
+                    }
+                    .frame(height: 40)
+                    
+                }
+            }
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
