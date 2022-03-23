43d87e084fbee510a4e778a2d9cb0094b13b9323
Ian <ianchen0419@gmail.com>
2022-03-23 12:14:47 +0800

    Update Day93-understanding-frames-and-coordinates-inside-geometryreader.swift

--- Day93-understanding-frames-and-coordinates-inside-geometryreader.swift
+++ Day93-understanding-frames-and-coordinates-inside-geometryreader.swift
@@ -7,61 +7,126 @@
 
 import SwiftUI
 
-// coordinate spaces
-// global name space: our views relative to the whole screen
-// local name space: relative to its immediate parent
-// custom coordinate space
-
-struct OuterView: View {
-    var body: some View {
-        VStack {
-            Text("Top")
-            InnerView()
-                .background(.green)
-            Text("Bottom")
-        }
-    }
-}
-
-struct InnerView: View {
-    var body: some View {
-        HStack {
-            Text("Left")
+struct BoxView: View {
+  
+  @State var blueOffset:CGFloat = -90
+  @State var blueDegree:Double = 180
+  
+  @State var redOffset: CGFloat = 0
+  @State var redDegree:Double = 90
+  
+  @State var greenOffset:CGFloat = 90
+  @State var greenDegree:Double = 90
+  
+  @State var orangeDegree: Double = 0
+  @State var orangeOffset: CGFloat = 0
+  
+  var body: some View {
+    VStack {
+      Text("Run")
+        .onTapGesture {
+          withAnimation(.linear(duration: 16)) {
+            self.blueDegree = 270
+            self.blueOffset = 0
             
-            GeometryReader { geo in
-                Text("Center")
-                    .background(.blue)
-                    .onTapGesture {
-                        print("Global center: \(geo.frame(in: .global).midX) x \(geo.frame(in: .global).midY)")
-                        print("Local center: \(geo.frame(in: .local).midX) x \(geo.frame(in: .local).midY)")
-                        print("Custom center: \(geo.frame(in: .named("Custom")).midX) x \(geo.frame(in: .named("Custom")).midY)")
-                    }
+            self.redDegree = 180
+            self.redOffset = 90
+            
+            self.greenDegree = 0
+            self.greenOffset = 0
+            
+            self.orangeDegree = 90
+            self.orangeOffset = -90
+          }
+      }
+      ZStack {
+        Spacer()
+        ZStack {
+          Text("6")
+            .offset(.init(width: 24, height: 0))
+          Rectangle()
+            .stroke(Color.blue, lineWidth: 5)
+            .opacity(0.2)
+            .frame(width: 90, height: 90, alignment: .center)
+        }.rotation3DEffect(.degrees(self.blueDegree), axis: (x: 0, y: -1, z: 0), anchor: UnitPoint.trailing, anchorZ: 0, perspective: -0.1)
+          .offset(CGSize(width: blueOffset, height: 0))
+        ZStack {
+          Text("2")
+            .offset(.init(width: -24, height: 0))
+          Rectangle()
+            .stroke(Color.red, lineWidth: 5)
+            .opacity(0.2)
+            .frame(width: 90, height: 90, alignment: .center)
+        }.rotation3DEffect(.degrees(self.redDegree), axis: (x: 0, y: -1, z: 0), anchor: UnitPoint.leading, anchorZ: 0, perspective: -0.1)
+        .offset(CGSize(width: redOffset, height: 0))
+        Spacer()
+
+        Spacer()
+        ZStack {
+          Text("5")
+            .onTapGesture {
+              print("greenDegree ",self.greenDegree)
+              print("greenOffset ",self.greenOffset)
             }
-            .background(.orange)
-            
-            Text("Right")
-        }
+            .offset(.init(width: 0, height: 24))
+          Rectangle()
+            .stroke(Color.green, lineWidth: 5)
+            .opacity(0.9)
+            .frame(width: 90, height: 90, alignment: .center)
+        }.rotation3DEffect(.degrees(greenDegree), axis: (x: 0, y: 1, z: 0), anchor: UnitPoint.leading, anchorZ: 0, perspective: 0.1)
+        .offset(CGSize(width: greenOffset, height: 0))
+        ZStack {
+          Text("1")
+            .offset(.init(width: 0, height: -24))
+          Rectangle()
+            .stroke(Color.orange, lineWidth: 5)
+            .opacity(0.9)
+            .frame(width: 90, height: 90, alignment: .center)
+        }.rotation3DEffect(.degrees(self.orangeDegree), axis: (x: 0, y: -1, z: 0), anchor: UnitPoint.trailing, anchorZ: 0, perspective: 0.1)
+        .offset(x: orangeOffset, y: 0)
+        
+        Spacer()
+      }
     }
+  
+  }
 }
 
 struct ContentView: View {
+    let colors: [Color] = [.red, .green, .blue, .orange, .pink, .purple, .yellow]
+    
     var body: some View {
-        
-//        VStack {
-//            GeometryReader { geo in // flexible preferred size: expand to take up more space as needed
-//                Text("Hello, world!")
-//                    .frame(width: geo.size.width * 0.9)
-//                    .background(.red)
+//        GeometryReader { fullView in
+//            ScrollView {
+//                ForEach(0..<50) { index in
+//                    GeometryReader { geo in
+//                        Text("Row #\(index)")
+//                            .font(.title)
+//                            .frame(maxWidth: .infinity)
+//                            .background(colors[index % 7])
+//                            .rotation3DEffect(.degrees(geo.frame(in: .global).minY - fullView.size.height / 2) / 5, axis: (x: 0, y: 1, z: 0))
+//                    }
+//                    .frame(height: 40)
+//                }
 //            }
-//            .background(.green)
-//
-//            Text("More text")
-//                .background(.blue)
 //        }
         
-        OuterView()
-            .background(.red)
-            .coordinateSpace(name: "Custom")
+        ScrollView(.horizontal, showsIndicators: false) {
+            HStack(spacing: 0) {
+                ForEach(1..<20) { num in
+                    GeometryReader { geo in
+                        Text("Number \(num)")
+                            .font(.largeTitle)
+                            .padding()
+                            .background(colors[num % 7])
+                            .rotation3DEffect(.degrees(-geo.frame(in: .global).minX) / 8, axis: (x: 0, y: 1, z: 0))
+                            .frame(width: 200, height: 200)
+                    }
+                    .frame(width: 200, height: 200)
+                }
+            }
+        }
+        
     }
 }
 
