dbe9931d0f11d511cfd004965f721ab9aa0f29cc
Yi An Chen <ianchen0419@gmail.com>
2022-03-23 14:14:55 +0800

    fix

--- Day93-understanding-frames-and-coordinates-inside-geometryreader.swift
+++ Day93-understanding-frames-and-coordinates-inside-geometryreader.swift
@@ -4,44 +4,62 @@
 //
 //  Created by Yi An Chen on 2022/3/22.
 //
-
 import SwiftUI
 
-struct ContentView: View {
-    let colors: [Color] = [.red, .green, .blue, .orange, .pink, .purple, .yellow]
-    
+// coordinate spaces
+// global name space: our views relative to the whole screen
+// local name space: relative to its immediate parent
+// custom coordinate space
+struct OuterView: View {
     var body: some View {
-//        GeometryReader { fullView in
-//            ScrollView {
-//                ForEach(0..<50) { index in
-//                    GeometryReader { geo in
-//                        Text("Row #\(index)")
-//                            .font(.title)
-//                            .frame(maxWidth: .infinity)
-//                            .background(colors[index % 7])
-//                            .rotation3DEffect(.degrees(geo.frame(in: .global).minY - fullView.size.height / 2) / 5, axis: (x: 0, y: 1, z: 0))
-//                    }
-//                    .frame(height: 40)
-//                }
+        VStack {
+            Text("Top")
+            InnerView()
+                .background(.green)
+            Text("Bottom")
+        }
+    }
+}
+
+struct InnerView: View {
+    var body: some View {
+        HStack {
+            Text("Left")
+            
+            GeometryReader { geo in
+                Text("Center")
+                    .background(.blue)
+                    .onTapGesture {
+                        print("Global center: \(geo.frame(in: .global).midX) x \(geo.frame(in: .global).midY)")
+                        print("Local center: \(geo.frame(in: .local).midX) x \(geo.frame(in: .local).midY)")
+                        print("Custom center: \(geo.frame(in: .named("Custom")).midX) x \(geo.frame(in: .named("Custom")).midY)")
+                    }
+            }
+            .background(.orange)
+            
+            Text("Right")
+        }
+    }
+}
+
+struct ContentView: View {
+    var body: some View {
+        
+//        VStack {
+//            GeometryReader { geo in // flexible preferred size: expand to take up more space as needed
+//                Text("Hello, world!")
+//                    .frame(width: geo.size.width * 0.9)
+//                    .background(.red)
 //            }
+//            .background(.green)
+//
+//            Text("More text")
+//                .background(.blue)
 //        }
         
-        ScrollView(.horizontal, showsIndicators: false) {
-            HStack(spacing: 0) {
-                ForEach(1..<20) { num in
-                    GeometryReader { geo in
-                        Text("Number \(num)")
-                            .font(.largeTitle)
-                            .padding()
-                            .background(colors[num % 7])
-                            .rotation3DEffect(.degrees(-geo.frame(in: .global).minX) / 8, axis: (x: 0, y: 1, z: 0))
-                            .frame(width: 200, height: 200)
-                    }
-                    .frame(width: 200, height: 200)
-                }
-            }
-        }
-        
+        OuterView()
+            .background(.red)
+            .coordinateSpace(name: "Custom")
     }
 }
 
